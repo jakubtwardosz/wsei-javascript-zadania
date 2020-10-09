4a0a7494aadf361261a2381fd8711f80122c7258
Jakub Twardosz <jtwardosz13@gmail.com>
2020-10-09 22:00:51 +0200

    1 task complete

---
+++ 1.Początki/script.js
@@ -0,0 +1,14 @@
+//Zadanie 1
+console.log("Jakub Twardosz");
+
+//Zadanie 2
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//Zadanie 3
+console.log("Hello World");
+// Nie ma takiej zmiennej
+//console.log(Hello World);
+// Brakuje cudzysłowu 
+//console.log("Hello World);
