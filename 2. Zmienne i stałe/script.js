b5fb1f79c285bbfe172f03d331e9f69213bcb211
Jakub Twardosz <jtwardosz13@gmail.com>
2020-11-01 10:57:06 +0100

    2 task complete

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,25 @@
+//Zadanie 1
+const exampleNumber = 12;
+const exampleText = "Funny text";
+const exampleNumberString = 2 + "two";
+const exampleBool = false;
+const exampleNull = null;
+
+console.log(exampleNumber);
+console.log(exampleText);
+console.log(exampleNumberString);
+console.log(exampleBool);
+console.log(exampleNull);
+//Zadanie 2
+
+const a = 10;
+const b = 23;
+let sum = 0;
+
+console.log(sum = a + b); 
+
+//Zadanie 3 
+
+let exampleVar;
+
+console.log(exampleVar); // wynik to undefined - do zmiennej nic nie jest przypisane
