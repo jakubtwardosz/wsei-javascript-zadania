2549643026cf81696ec3e2beeb0dbac7a1bd9cd9
Jakub Twardosz <jtwardosz13@gmail.com>
2020-11-01 22:47:58 +0100

    3 done

---
+++ 3.Operatory/script.js
@@ -0,0 +1,60 @@
+// Zadanie 1 
+
+let example1 = true;
+let example2 = false;
+
+console.log(example2 == example1);
+
+// Zadanie 2
+
+let number1 = 12;
+let number2 = 1276;
+let moduloResult = 0;
+
+moduloResult = number1 % number2;
+
+console.log(moduloResult);
+
+// Zadanie 3 
+
+let string1 = "Ala";
+let string2 = " ma kota";
+let stringsResult = "";
+
+stringsResult = string1 + string2;
+
+console.log(stringsResult);
+
+// Zadanie 4 
+
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString); // ponieważ mają taką samą wartość
+console.log(someNumber === someString); // ponieważ mają różne typy
+
+// Zadanie 5
+
+let counter = 30;
+
+console.log(counter);
+
+counter++;
+
+console.log(counter);
+
+counter--;
+
+console.log(counter);
+
+// Zadanie 6
+
+let number3 = 4543;
+
+let number4 = 843;
+
+let result = null;
+
+result = number3 > number4;
+
+console.log(result);
